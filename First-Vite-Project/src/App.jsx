b5be2907e1fc6179c30092b19f5176eb37f2585f
Paul De Ulloa <pauld143@gmail.com>
2024-03-28 00:17:49 -0700

    app.jsx and main.jsx file updates

--- First-Vite-Project/src/App.jsx
+++ First-Vite-Project/src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-dom";
+import Duel from "./components/Duel.jsx";
 
 function App() {
   return (
     <>
       <p>Hello Vite World!</p>
+      <Routes>
+        <Route path="/" element={<Duel />} />
+      </Routes>
     </>
   );
 }
